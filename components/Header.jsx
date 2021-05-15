e9099868b762549e2e07cd91cb02a2a8265ff03b
Rakshit <rakshit@mobifyi.com>
2021-05-15 16:26:55 +0530

    added bid details page

---
+++ components/Header.jsx
@@ -0,0 +1,25 @@
+import { Main_Logo, THEME_COLOR } from "../lib/config";
+import _JSXStyle from "styled-jsx/style";
+
+function CustomHeader(props) {
+  return (
+    <>
+      <div className="col-12 p-3">
+        <img src={Main_Logo} alt="Main logo" width={110} height={45} />
+      </div>
+      <div className="d-flex align-items-center justify-content-center headerContainer">
+        <p>{props.title}</p>
+      </div>
+      <style jsx>{`
+        .headerContainer {
+          height: 25vh;
+          background: ${THEME_COLOR};
+          color: white;
+          font-size: 8vh;
+        }
+      `}</style>
+    </>
+  );
+}
+
+export default CustomHeader;
